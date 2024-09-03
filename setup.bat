885f7caa976f28488edf0efc7b4c18f8b34d2123
Oussama <wildric.auric@gmail.com>
2024-09-03 10:50:49 +0200

    update for cloning

--- setup.bat
+++ setup.bat
@@ -13,3 +13,13 @@ git clone https://github.com/Wildric-Auric/LWmath.git
 move LWmath .\vendor\lwmath
 
 premake5 vs2022
+
+mkdir build
+mkdir .\build\bin
+
+glslc src\shdrs\tri.vert  -o build/bin/trivert.spv
+glslc src\shdrs\tri.frag  -o build/bin/trifrag.spv
+glslc src\shdrs\test.comp -o build/bin/testcomp.spv
+glslc src\shdrs\test.tesc -o build/bin/testtesc.spv
+glslc src\shdrs\test.tese -o build/bin/testtese.spv
+glslc src\shdrs\test.geom -o build/bin/testgeom.spv
